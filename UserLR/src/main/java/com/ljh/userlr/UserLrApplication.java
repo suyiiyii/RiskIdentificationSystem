aece9f71d574db9cc419cf7340e6999da6937076
lijiahong <488241817@qq.com>
2024-07-17 16:38:31 +0800

    建好项目，建好用户登录注册的模块，完成分包，Controller、Services、pojo、mapper、view包，pojo中写好了User实体类

---
+++ UserLR/src/main/java/com/ljh/userlr/UserLrApplication.java
@@ -0,0 +1,13 @@
+package com.ljh.userlr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserLrApplication.class, args);
+    }
+
+}
---
+++ UserLR/src/test/java/com/ljh/userlr/UserLrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.userlr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
